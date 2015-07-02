a328a1338bc758a8aa7f6e0587cad4757dddd22b
CodyGuo <guoxiao219@126.com>
2015-07-02 08:09:34 +0800

    004 用Sublime Text3打开文件

---
+++ 1chapter/004/main.go
@@ -0,0 +1,32 @@
+/*
+ * @example: 用Sublime Text3打开文件
+ * @author: cody.guo
+ */
+
+1> 使用Sublime Text3编写Go代码
+2> 安装git工具
+3> 在Sublime上安装插件，输入Ctrl + Shift + P，然后输出install
+4> 安装支持Go语言的插件：GoSublime
+5> 安装支持git的插件：SublimeGit
+6> 在命令行中输入git config --global credential.helper store
+
+
+C:\Users\Administrator>git config --list
+core.symlinks=false
+core.autocrlf=true
+color.diff=auto
+color.status=auto
+color.branch=auto
+color.interactive=true
+pack.packsizelimit=2g
+help.format=html
+http.sslcainfo=/bin/curl-ca-bundle.crt
+sendemail.smtpserver=/bin/msmtp.exe
+diff.astextplain.textconv=astextplain
+rebase.autosquash=true
+user.name=CodyGuo
+user.email=guoxiao219@126.com
+push.default=matching
+gui.recentrepo=D:/go/cpath/src/282example
+credential.helper=store
+http.sslverify=false
